35637b7a7c5381e18a57934dbe565206283f1f61
John McCann <danger.mccann@gmail.com>
2015-12-28 22:53:22 -0500

    styled some of the ui elements

--- Assets/UI/StatsPanel.cs
+++ Assets/UI/StatsPanel.cs
@@ -5,14 +5,9 @@ using Lords;
 public class StatsPanel : MonoBehaviour {
 
 
-	Text population, funds, materials, happiness, propsperity, culture; 
+	Text population; 
 	void Start() {
 		population = GetUIText("PopulationValue");
-		funds = GetUIText("FundsValue");
-		materials = GetUIText("MaterialsValue");
-		happiness = GetUIText("HappinessValue");
-		propsperity = GetUIText("ProsperityValue");
-		culture = GetUIText("CultureValue");
 	}
 
 	Text GetUIText(string name) {
@@ -21,10 +16,5 @@ public class StatsPanel : MonoBehaviour {
 
 	void Update () {
 		population.text = Mathf.Floor(Game.CurrentCity.Score.Population).ToString();
-		funds.text = Mathf.Floor(Game.CurrentCity.Funds).ToString();
-		materials.text = Mathf.Floor(Game.CurrentCity.RawMaterials).ToString();
-		happiness.text = Mathf.Floor(Game.CurrentCity.Score.Happiness).ToString();
-		propsperity.text = Mathf.Floor(Game.CurrentCity.Score.Prosperity).ToString();
-		culture.text = Mathf.Floor(Game.CurrentCity.Score.Culture).ToString();
 	}
 }

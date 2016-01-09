3fb82aa075d347dfe68454b5e7eae0f68f9374d8
John McCann <danger.mccann@gmail.com>
2016-01-09 15:10:13 -0500

    Started working on map scene

---
+++ Assets/UI/MapNavigator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Lords {
+	public class MapNavigator : MonoBehaviour {
+		void Start () {
+		
+		}
+		
+
+		void Update () {
+			float tt = transform.localScale.x;
+			tt += Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 400;
+			tt = Mathf.Clamp(tt, 1, 3);
+			transform.localScale = new Vector3(tt, tt, tt);
+		}
+	}
+}
